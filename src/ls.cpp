0f20102fcf80683df04d19f7487dbc5712eec883
Sean Villagomez <svill014@well.cs.ucr.edu>
2014-08-12 10:15:03 -0700

    can now tell if files are called with the program

--- src/ls.cpp
+++ src/ls.cpp
@@ -4,6 +4,8 @@
 
 #include <pwd.h>
 
+#include <vector>
+
 //color
 #include <cstdio>
 
@@ -18,10 +20,12 @@ using namespace std;
 
 int main(int argv, char* argc[])
 {
-	
+	vector<char*> files;
 	bool l=false;
         bool a=false;
         bool R=false;
+
+	bool all=true;
 	for(int j=0; j<argv; j++)
 	{
 		if(argc[j][0]=='-')
@@ -36,9 +40,21 @@ int main(int argv, char* argc[])
                                         R=true;
 			}
 		}
+		else
+		{
+			files.push_back(argc[j]);
+		}
 	}
-
-
+	/////////////////////////
+	for(int a=0; a<files.size(); a++)
+	{
+		if(files.size()>1)
+			all = false;
+		cout << files.at(a) << endl;
+	}
+	///////////////////////////////
+	cout << endl;
+	cout << all << " :\n";
 	char *dirName = ".";
 	DIR *dirp;
 	if (!(dirp  = opendir(dirName)))
@@ -53,24 +69,29 @@ int main(int argv, char* argc[])
 	int color =0;//if 1 it will be blue, if 2 it will be green
 
 	dirent *direntp;
-	
-
-
 	while ((direntp = readdir(dirp)))
 	{
 		stat(direntp->d_name,&s);
 	        int mode = s.st_mode;
+	    if(all)
+	    {
 		if(!a)
 		{
+		 //// int num=files.size();
+		 //// if((fSize<num)||num<=1)
+		 //// {
+		 ////   if((files.at(fSize)==(string)direntp->d_name)||(num<=1))
+		 ////   {
 			if((l)&&(direntp->d_name[0]!='.'))
 			{
+			//	if(files.at(fSize)==direntp->d_name)
 				if(S_ISDIR(s.st_mode))
-                                {
-                                        color =1;
-                                        cout << 'd';
-                                }
-                                else
-                                        cout << '-';
+				{
+					color =1;
+					cout << 'd';
+				}
+				else
+					cout << '-';
 				if(mode&S_IRUSR)
 					cout << 'r';
 				else
@@ -173,30 +194,30 @@ int main(int argv, char* argc[])
 				if(color==0)
 				{
 					if(direntp->d_name[0]=='.')
-                	                	printf("%c[%dm", 0x1B,47);
-		                        cout << direntp->d_name;
+						printf("%c[%dm", 0x1B,47);
+					cout << direntp->d_name;
 					printf("%c[%dm", 0x1B,39);
-                                        printf("%c[%dm", 0x1B,49);
-                                        cout << "\n";
+					printf("%c[%dm", 0x1B,49);
+					cout << "\n";
 				}
 				else if(color==1)
 				{
 					if(direntp->d_name[0]=='.')
-                		                printf("%c[%dm", 0x1B,47);
+						printf("%c[%dm", 0x1B,47);
 					printf("%c[%dm", 0x1B,34);
 					cout << direntp->d_name;
 					printf("%c[%dm", 0x1B,39);
-                                        printf("%c[%dm", 0x1B,49);
-                                        cout << "/\n";
+					printf("%c[%dm", 0x1B,49);
+					cout << "/\n";
 				}
 				else if(color==2)
 				{
 					if(direntp->d_name[0]=='.')
-                                                printf("%c[%dm", 0x1B,47);
-                                        printf("%c[%dm", 0x1B,32);
-                                        cout << direntp->d_name;
+						printf("%c[%dm", 0x1B,47);
+					printf("%c[%dm", 0x1B,32);
+					cout << direntp->d_name;
 					printf("%c[%dm", 0x1B,39);
-		                        printf("%c[%dm", 0x1B,49);
+					printf("%c[%dm", 0x1B,49);
 					cout << "*\n";
 				}
 				color =0;
@@ -204,9 +225,9 @@ int main(int argv, char* argc[])
 			else
 			{
 				if(S_ISDIR(s.st_mode))
-                                {
-                                        color =1;
-                                }
+				{
+					color =1;
+				}
 				if(mode&S_IXUSR)
 				{
 					if( color==0)
@@ -234,48 +255,49 @@ int main(int argv, char* argc[])
 				if(color==0)
 				{
 					if(direntp->d_name[0]!='.')
-                                        {
-                                                cout << direntp->d_name;
-                                                printf("%c[%dm", 0x1B,39);
-                                                printf("%c[%dm", 0x1B,49);
-                                                cout << " ";
-                                        }
+					{
+						cout << direntp->d_name;
+						printf("%c[%dm", 0x1B,39);
+						printf("%c[%dm", 0x1B,49);
+						cout << " ";
+					}
 				}
 				else if(color==1)
 				{
 					printf("%c[%dm", 0x1B,34);
 					if(direntp->d_name[0]!='.')
-                                        {
-                                                cout << direntp->d_name;
-                                                printf("%c[%dm", 0x1B,39);
-                                                printf("%c[%dm", 0x1B,49);
-                                                cout << "/ ";
-                                        }
+					{
+						cout << direntp->d_name;
+						printf("%c[%dm", 0x1B,39);
+						printf("%c[%dm", 0x1B,49);
+						cout << "/ ";
+					}
 				}
 				else if(color==2)
 				{
-                                        printf("%c[%dm", 0x1B,32);
+					printf("%c[%dm", 0x1B,32);
 					if(direntp->d_name[0]!='.')
-		                        {
-                		                cout << direntp->d_name;
-		                                printf("%c[%dm", 0x1B,39);
-		                                printf("%c[%dm", 0x1B,49);
-		                                cout << "* ";
-                        		}
+					{
+						cout << direntp->d_name;
+						printf("%c[%dm", 0x1B,39);
+						printf("%c[%dm", 0x1B,49);
+						cout << "* ";
+					}
 				}
 				color =0;
 				printf("%c[%dm", 0x1B,39);
 				printf("%c[%dm", 0x1B,49);
 			}
+		}
 /*			if(direntp->d_name[0]!='.')
 			{
 				cout << direntp->d_name;
 				printf("%c[%dm", 0x1B,39);
-        	                printf("%c[%dm", 0x1B,49);
-	                        cout << " ";
+				printf("%c[%dm", 0x1B,49);
+				cout << " ";
 
-			}
-*/		}
+			}*/
+			
 		else
 		{
 			if(l)
@@ -488,9 +510,450 @@ int main(int argv, char* argc[])
 				printf("%c[%dm", 0x1B,49);
 			}
 		}
+	    }
+	    else//this is called if files are also being called
+	    {
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		if(!a)
+		{
+		 //// int num=files.size();
+		 //// if((fSize<num)||num<=1)
+		 //// {
+		 ////   if((files.at(fSize)==(string)direntp->d_name)||(num<=1))
+		 ////   {
+			if((l)&&(direntp->d_name[0]!='.'))
+			{
+			//	if(files.at(fSize)==direntp->d_name)
+				if(S_ISDIR(s.st_mode))
+				{
+					color =1;
+					cout << 'd';
+				}
+				else
+					cout << '-';
+				if(mode&S_IRUSR)
+					cout << 'r';
+				else
+					cout << '-';
+				if(mode&S_IWUSR)
+					cout << 'w';
+				else 
+					cout << '-';
+				if(mode&S_IXUSR)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				else 
+					cout << '-';
+				if(mode&S_IRGRP)
+					cout << 'r';
+				else 
+					cout << '-';
+				if(mode&S_IWGRP)
+					cout << 'w';
+				else 
+					cout << '-';
+				if(mode&S_IXGRP)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				else
+					cout << '-';
+				if(mode&S_IROTH)
+					cout << 'r';
+				else
+					cout << '-';
+				if(mode&S_IWOTH)
+					cout << 'w';
+				else
+					cout << '-';
+				if(mode&S_IXOTH)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				else
+					cout << '-';
+
+				cout << " " << s.st_nlink << " ";
+				struct passwd *name;
+				if (getpwuid(s.st_uid)==NULL)
+				{
+					perror("getpwuid failed");
+					exit(0);
+				}
+				name = getpwuid(s.st_uid);
+				cout << name->pw_name<<" ";
+				
+				string temp = name->pw_dir;
+				string temp2="";
+				int slash =0;
+				int i=0;
+				while(slash<3)
+				{
+					if(temp[i]=='/')
+					{
+						slash++;
+					}
+					else if(slash==2)
+					{
+						temp2=temp2+temp[i];
+					}
+					i++;
+				}
+				cout << temp2 << ' ';
+				int size=s.st_size;
+				if(size>10000)
+					cout << size << ' ';
+				else if (size>=1000)
+					cout <<" " << size << ' ';
+				else if (size>=100)
+					cout <<"  " << size << ' ';
+				else if (size>=10)
+					cout <<"   " << size << ' ';
+				else if (size>=0)
+					cout <<"    " << size << ' ';
+				struct tm* time;
+				char buff[20];
+				time = localtime(&(s.st_mtime));
+				strftime(buff,20, "%b %d %H:%M", time);
+				printf("%s",buff);
+				cout << ' ';
+				if(color==0)
+				{
+					if(direntp->d_name[0]=='.')
+						printf("%c[%dm", 0x1B,47);
+					cout << direntp->d_name;
+					printf("%c[%dm", 0x1B,39);
+					printf("%c[%dm", 0x1B,49);
+					cout << "\n";
+				}
+				else if(color==1)
+				{
+					if(direntp->d_name[0]=='.')
+						printf("%c[%dm", 0x1B,47);
+					printf("%c[%dm", 0x1B,34);
+					cout << direntp->d_name;
+					printf("%c[%dm", 0x1B,39);
+					printf("%c[%dm", 0x1B,49);
+					cout << "/\n";
+				}
+				else if(color==2)
+				{
+					if(direntp->d_name[0]=='.')
+						printf("%c[%dm", 0x1B,47);
+					printf("%c[%dm", 0x1B,32);
+					cout << direntp->d_name;
+					printf("%c[%dm", 0x1B,39);
+					printf("%c[%dm", 0x1B,49);
+					cout << "*\n";
+				}
+				color =0;
+			}
+			else
+			{
+				if(S_ISDIR(s.st_mode))
+				{
+					color =1;
+				}
+				if(mode&S_IXUSR)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+				}
+				if(mode&S_IXGRP)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				if(mode&S_IXOTH)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				
+				if(color==0)
+				{
+					if(direntp->d_name[0]!='.')
+					{
+						cout << direntp->d_name;
+						printf("%c[%dm", 0x1B,39);
+						printf("%c[%dm", 0x1B,49);
+						cout << " ";
+					}
+				}
+				else if(color==1)
+				{
+					printf("%c[%dm", 0x1B,34);
+					if(direntp->d_name[0]!='.')
+					{
+						cout << direntp->d_name;
+						printf("%c[%dm", 0x1B,39);
+						printf("%c[%dm", 0x1B,49);
+						cout << "/ ";
+					}
+				}
+				else if(color==2)
+				{
+					printf("%c[%dm", 0x1B,32);
+					if(direntp->d_name[0]!='.')
+					{
+						cout << direntp->d_name;
+						printf("%c[%dm", 0x1B,39);
+						printf("%c[%dm", 0x1B,49);
+						cout << "* ";
+					}
+				}
+				color =0;
+				printf("%c[%dm", 0x1B,39);
+				printf("%c[%dm", 0x1B,49);
+			}
+		}
+/*			if(direntp->d_name[0]!='.')
+			{
+				cout << direntp->d_name;
+				printf("%c[%dm", 0x1B,39);
+				printf("%c[%dm", 0x1B,49);
+				cout << " ";
+
+			}*/
+			
+		else
+		{
+			if(l)
+			{
+				if(S_ISDIR(s.st_mode))
+                                {
+                                        color =1;
+                                        cout << 'd';
+                                }
+                                else
+                                        cout << '-';
+				if(mode&S_IRUSR)
+					cout << 'r';
+				else
+					cout << '-';
+				if(mode&S_IWUSR)
+					cout << 'w';
+				else 
+					cout << '-';
+				if(mode&S_IXUSR)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				else 
+					cout << '-';
+				if(mode&S_IRGRP)
+					cout << 'r';
+				else 
+					cout << '-';
+				if(mode&S_IWGRP)
+					cout << 'w';
+				else 
+					cout << '-';
+				if(mode&S_IXGRP)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				else
+					cout << '-';
+				if(mode&S_IROTH)
+					cout << 'r';
+				else
+					cout << '-';
+				if(mode&S_IWOTH)
+					cout << 'w';
+				else
+					cout << '-';
+				if(mode&S_IXOTH)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				else
+					cout << '-';
+
+				cout << " " << s.st_nlink << " ";
+				struct passwd *name;
+				if (getpwuid(s.st_uid)==NULL)
+				{
+					perror("getpwuid failed");
+					exit(0);
+				}
+				name = getpwuid(s.st_uid);
+				cout << name->pw_name<<" ";
+				
+				string temp = name->pw_dir;
+				string temp2="";
+				int slash =0;
+				int i=0;
+				while(slash<3)
+				{
+					if(temp[i]=='/')
+					{
+						slash++;
+					}
+					else if(slash==2)
+					{
+						temp2=temp2+temp[i];
+					}
+					i++;
+				}
+				cout << temp2 << ' ';
+				int size=s.st_size;
+				if(size>10000)
+					cout << size << ' ';
+				else if (size>=1000)
+					cout <<" " << size << ' ';
+				else if (size>=100)
+					cout <<"  " << size << ' ';
+				else if (size>=10)
+					cout <<"   " << size << ' ';
+				else if (size>=0)
+					cout <<"    " << size << ' ';
+				struct tm* time;
+				char buff[20];
+				time = localtime(&(s.st_mtime));
+				strftime(buff,20, "%b %d %H:%M", time);
+				printf("%s",buff);
+				cout << ' ';
+				if(color==0)
+				{
+					if(direntp->d_name[0]=='.')
+                	                	printf("%c[%dm", 0x1B,47);
+		                        cout << direntp->d_name;
+					printf("%c[%dm", 0x1B,39);
+                                        printf("%c[%dm", 0x1B,49);
+                                        cout << "\n";
+				}
+				else if(color==1)
+				{
+					if(direntp->d_name[0]=='.')
+                		                printf("%c[%dm", 0x1B,47);
+					printf("%c[%dm", 0x1B,34);
+					cout << direntp->d_name;
+					printf("%c[%dm", 0x1B,39);
+                                        printf("%c[%dm", 0x1B,49);
+                                        cout << "/\n";
+				}
+				else if(color==2)
+				{
+					if(direntp->d_name[0]=='.')
+                                                printf("%c[%dm", 0x1B,47);
+                                        printf("%c[%dm", 0x1B,32);
+                                        cout << direntp->d_name;
+					printf("%c[%dm", 0x1B,39);
+		                        printf("%c[%dm", 0x1B,49);
+					cout << "*\n";
+				}
+				color =0;
+			}
+			else
+			{
+
+				
+				if(S_ISDIR(s.st_mode))
+                                {
+                                        color =1;
+                                }
+				if(mode&S_IXUSR)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+				}
+				if(mode&S_IXGRP)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				if(mode&S_IXOTH)
+				{
+					if( color==0)
+					{
+						color =2;
+					}
+					cout << 'x';
+				}
+				
+
+
+				if(direntp->d_name[0]=='.')
+				printf("%c[%dm", 0x1B,47);
+			
+				if(color==0)
+				{
+					if(direntp->d_name[0]=='.')
+                	                	printf("%c[%dm", 0x1B,47);
+		                        cout << direntp->d_name;
+					printf("%c[%dm", 0x1B,39);
+                                        printf("%c[%dm", 0x1B,49);
+                                        cout << " ";
+				}
+				else if(color==1)
+				{
+					if(direntp->d_name[0]=='.')
+                		                printf("%c[%dm", 0x1B,47);
+					printf("%c[%dm", 0x1B,34);
+					cout << direntp->d_name;
+					printf("%c[%dm", 0x1B,39);
+                                        printf("%c[%dm", 0x1B,49);
+                                        cout << "/ ";
+				}
+				else if(color==2)
+				{
+					if(direntp->d_name[0]=='.')
+                                                printf("%c[%dm", 0x1B,47);
+                                        printf("%c[%dm", 0x1B,32);
+                                        cout << direntp->d_name;
+					printf("%c[%dm", 0x1B,39);
+		                        printf("%c[%dm", 0x1B,49);
+					cout << "* ";
+				}
+				color =0;
+				printf("%c[%dm", 0x1B,39);
+				printf("%c[%dm", 0x1B,49);
+			}
+		}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	    }
 	}
 	closedir(dirp);
-
+	
 	return 0;
 }
 
